070fb0ce0b8fe6133f09c1d87412251fb290718c
RJ <szarleydwarf@gmail.com>
2019-03-19 17:17:51 +0000

    designing of the settings activity view.

--- app/src/main/java/eu/rjch/kalkulatorcen/activities/AppSetup.java
+++ app/src/main/java/eu/rjch/kalkulatorcen/activities/AppSetup.java
@@ -3,6 +3,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import eu.rjch.kalkulatorcen.R;
 
 public class AppSetup extends Activity {
@@ -10,9 +12,9 @@ public class AppSetup extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			setContentView(R.layout.app_layout);
+			setContentView(R.layout.app_setup_layout);
 		}else {
-			setContentView(R.layout.app_layout_old);
+			setContentView(R.layout.app_setup_layout_old);
 			
 		}
 		
@@ -21,9 +23,7 @@ public class AppSetup extends Activity {
 	}
 	
 	private void init() {
-		Intent i = new Intent(this, TheApp.class);
-		startActivity(i);
-		finish();
+		Button save = findViewById(R.id.save_btn);
 		//todo
 		/* design settings Window
 		*  think what options to allow to modify
@@ -31,5 +31,13 @@ public class AppSetup extends Activity {
 		*  or
 		*  pass them ?
 		* */
+		save.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent i = new Intent(view.getContext(), TheApp.class);
+				startActivity(i);
+				finish();
+			}
+		});
 	}
 }
